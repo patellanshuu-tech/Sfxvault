d2d715a4086eb1003f94c7ff0ca8d8b577ecc588
patellanshuu-tech <patellanshuu@gmail.com>
2026-09-13 22:06:55 +0530

    Delete lib directory

--- lib/db.ts
+++
@@ -1,4 +0,0 @@
-import { Pool } from "pg";
-declare global { var sfxPool: Pool | undefined; }
-export const pool = global.sfxPool ?? new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
-if (process.env.NODE_ENV !== "production") global.sfxPool = pool;
