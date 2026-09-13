2bea89928f25cb2f0cb84e6a5d5b1b186c8c8227
patellanshuu-tech <patellanshuu@gmail.com>
2026-09-13 22:09:53 +0530

    Add files via upload

--- lib/db.ts
+++ lib/db.ts
@@ -1,1 +1,4 @@
-
+import { Pool } from "pg";
+declare global { var sfxPool: Pool | undefined; }
+export const pool = global.sfxPool ?? new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+if (process.env.NODE_ENV !== "production") global.sfxPool = pool;
